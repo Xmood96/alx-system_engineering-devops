4a61f06bdf60bc88046ba3a029ce0bb6da0556fb
mohammed_ali <102557625+Xmood96@users.noreply.github.com>
2022-11-26 11:51:34 +0300

    css files
    css

---
+++ rubic.js
@@ -0,0 +1,48 @@
+/*!
+=========================================================
+* Rubic Landing page
+=========================================================
+
+* Copyright: 2019 DevCRUD (https://devcrud.com)
+* Licensed: (https://devcrud.com/licenses)
+* Coded by www.devcrud.com
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// smooth scroll
+$(document).ready(function(){
+	$(".nav-link").on('click', function(event) {
+
+    	if (this.hash !== "") {
+
+			event.preventDefault();
+
+			var hash = this.hash;
+
+			$('html, body').animate({
+				scrollTop: $(hash).offset().top
+			}, 700, function(){
+				window.location.hash = hash;
+			});
+      	} 
+    });
+});
+let camera_button = document.querySelector("#start-camera");
+let video = document.querySelector("#video");
+let click_button = document.querySelector("#click-photo");
+let canvas = document.querySelector("#canvas");
+
+camera_button.addEventListener('click', async function() {
+   	let stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
+	video.srcObject = stream;
+});
+
+click_button.addEventListener('click', function() {
+   	canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+   	let image_data_url = canvas.toDataURL('image/jpeg');
+
+   	// data url of the image
+   	console.log(image_data_url);
